3ba366014682444a442103c1f2bdd6201efcb2e9
berger <robertberger.dev@gmail.com>
2023-03-13 17:52:34 -0400

    added to renders the name test

--- src/__tests__/Contact.test.js
+++ src/__tests__/Contact.test.js
@@ -5,3 +5,9 @@ import Contact from '../Contact';
 it('renders without error', () => {
   render(<Contact />)
 })
+
+it('renders the name', () => {
+    let contact = { name: "John Doe"}
+    render(<Contact contact={contact} />)
+    expect(screen.getByText('John Doe')).toBeInTheDocument()
+})
