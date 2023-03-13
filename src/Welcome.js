bef2ad7ecf551b9521284bf1fe2e0dbe4fa454ec
berger <robertberger.dev@gmail.com>
2023-03-13 11:45:56 -0400

    created Welcome.test.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,8 +1,14 @@
 import UserName from './UserName'
 
 function Welcome() {
+    function currentDate() {
+        return new Date(Date.now()).toLocaleString()
+    }
   return (
+    <>
     <h1 role="heading">Welcome back, <UserName /></h1>
+    <div data-testid="footer">This page was rendered on {currentDate()}</div>
+    </>
   )
 }
 export default Welcome
--- src/__tests__/Welcome.test.js
+++ src/__tests__/Welcome.test.js
@@ -10,3 +10,23 @@ it('displays the user name in the heading', () => {
   render(<Welcome />)
   expect(screen.getByRole('heading', {name: 'Welcome back, Test User'})).toBeInTheDocument();
 })
+
+afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  describe('the welcome footer', () => {
+    it('retrieves the current date', () => {
+      const spy = jest.spyOn(Date, 'now')
+      render(<Welcome />)
+      expect(spy).toHaveBeenCalled()
+    })
+  
+    it('displays the current date', () => {
+      const spy = jest.spyOn(Date, 'now')
+        .mockImplementation(() => new Date('2021-01-01T00:00')
+      )
+      render(<Welcome />)
+      expect(screen.getByText('This page was rendered on 1/1/2021, 12:00:00 AM')).toBeInTheDocument()
+    })
+  })
