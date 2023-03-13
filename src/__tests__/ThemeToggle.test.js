923092b4704d84a5fd276c2984d8397ac4ed8a59
berger <robertberger.dev@gmail.com>
2023-03-13 11:26:14 -0400

    added more example tests and no longer works

--- src/__tests__/ThemeToggle.test.js
+++ src/__tests__/ThemeToggle.test.js
@@ -2,7 +2,57 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ThemeToggle from '../ThemeToggle';
 
-it('renders a Light button', () => {
+beforeEach(() => {
     render(<ThemeToggle />)
+  })
+
+  it('defaults to light', () => {
+    expect(screen.getByLabelText('Light')).toBeChecked()
+  })
+  
+  it('renders a Light button', () => {
     expect(screen.getByText('Light')).toBeInTheDocument();
-})
+  })
+  
+  it('renders a Dark button', () => {
+    expect(screen.getByText('Dark')).toBeInTheDocument();
+  })
+
+  describe('when no default is supplied', () => {
+    beforeEach(() => {
+      render(<ThemeToggle />)
+    })
+  
+    it('renders a Light button', () => {
+      expect(screen.getByText('Light')).toBeInTheDocument();
+    })
+  
+    it('renders a Dark button', () => {
+      expect(screen.getByText('Dark')).toBeInTheDocument();
+    })
+  
+    it('defaults to light', () => {
+      expect(screen.getByLabelText('Light')).toBeChecked()
+    })
+  
+    it('changes to dark when clicked', () => {
+      fireEvent.click(screen.getByText('Dark'))
+      expect(screen.getByLabelText('Dark')).toBeChecked()
+    })
+  })
+  
+  describe('when dark is the default', () => {
+    beforeEach(() => {
+      render(<ThemeToggle defaultTheme="dark" />)
+    })
+  
+    it('defaults to dark', () => {
+      render(<ThemeToggle defaultTheme="dark" />)
+      expect(screen.getByLabelText('Dark')).toBeChecked()
+    })
+  
+    it('changes to light when clicked', () => {
+        fireEvent.click(screen.getByText('Light'))
+        expect(screen.getByLabelText('Light')).toBeChecked()
+     })
+  })
